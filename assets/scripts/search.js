712b5b46cf8741ddf83b6f8c5e2f7dfe591042b3
Gianlu201 <gianlucadidiego2001@gmail.com>
2024-12-18 23:46:13 +0100

    search and music player upgrade

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -23,9 +23,11 @@ async function search(q) {
   try {
     const response = await fetch(baseUrl + q);
     const data = await response.json();
-    console.log(data);
-    filterTracks(data.data);
-    // displayResults(data.data);
+    if (data.data[0] == undefined) {
+      noMatch();
+    } else {
+      filterTracks(data.data);
+    }
   } catch (error) {
     console.log(error);
   }
@@ -43,8 +45,7 @@ function filterTracks(tracks) {
 
   switch (myFilter) {
     case 'all':
-    default:
-      displayResults(tracks);
+      tracks.forEach((track) => myTracks.push(track));
       break;
     case 'artist':
       tracks.forEach((track) => {
@@ -86,23 +87,84 @@ function filterTracks(tracks) {
 
 function displayResults(tracks) {
   resultContainer.innerHTML = '';
+  titleSfoglia.style.display = 'none';
+  sectionSfoglia.style.display = 'none';
 
   tracks.forEach((track) => {
-    const trackCard = document.createElement('div');
-    trackCard.classList.add('col');
+    // const trackCard = document.createElement('div');
+    // trackCard.classList.add('col');
 
-    trackCard.innerHTML = `
-        <div class="card shadow-sm custom-card">
-          <img src="${track.album.cover_big}" class="card-img-fluid" alt="${track.album.title}">
-          <div class="card-body">
-            <h5 class="card-title">${track.title}</h5>
-            <p class="card-text">${track.artist.name}</p>
-          </div>
-        </div>
-      `;
+    // trackCard.innerHTML = `
+    //       <div class="card shadow-sm custom-card">
+    //       <a href="album.html?id=${track.album.id}">
+    //       <img src="${track.album.cover_big}" class="card-img-fluid" alt="${track.album.title}">
+    //       </a>
+    //         <div class="card-body">
+    //           <h5 class="card-title" onclick="setPlayer("${track.preview}", "${track.title}", "${track.artist.name}", "${track.album.cover_small}")">${track.title}</h5>
+    //           <a href="artistPage.html?id=${track.artist.id}">
+    //           <p class="card-text">${track.artist.name}</p>
+    //           </a>
+    //         </div>
+    //       </div>
+    //     `;
+
+    const trackCard = document.createElement('div');
+    trackCard.classList.add('card', 'shadow-sm', 'custom-card');
+
+    const newAlbumLink = document.createElement('a');
+    newAlbumLink.href = `album.html?id=${track.album.id}`;
+
+    const newImg = document.createElement('img');
+    newImg.src = track.album.cover_big;
+    newImg.classList.add('card-img-fluid', 'me-2');
+    newImg.alt = track.album.title;
+
+    const newBody = document.createElement('div');
+    newBody.classList.add('card-body');
+
+    const newTitle = document.createElement('h5');
+    newTitle.classList.add('card-title');
+    newTitle.innerText = track.title;
+    newTitle.setAttribute(
+      'onclick',
+      `setPlayer("${track.preview}", "${track.title}", "${track.artist.name}", "${track.album.cover_small}")`
+    );
+
+    const newArtistLink = document.createElement('a');
+    newArtistLink.href = `artistPage.html?id=${track.artist.id}`;
+
+    const newArtist = document.createElement('p');
+    newArtist.innerText = track.artist.name;
+
+    newAlbumLink.appendChild(newImg);
+    trackCard.appendChild(newAlbumLink);
+    newBody.appendChild(newTitle);
+    newArtistLink.appendChild(newArtist);
+    newBody.appendChild(newArtistLink);
+    trackCard.appendChild(newBody);
 
     resultContainer.appendChild(trackCard);
   });
+}
+
+function noMatch() {
+  resultContainer.innerHTML = '';
   titleSfoglia.style.display = 'none';
   sectionSfoglia.style.display = 'none';
+
+  const p = document.createElement('p');
+  p.innerText = 'Nessun risultato corrispondente';
+  p.classList.add('lead');
+
+  resultContainer.appendChild(p);
+}
+
+function setPlayer(link, title, artist, imgUrl) {
+  musicSource.innerHTML = '';
+  musicSource.src = link;
+  musicSource.setAttribute('autoplay', 'true');
+
+  document.getElementById('trackImage').src = imgUrl;
+  document.getElementById('trackName').innerText = `${title.slice(0, 16)}...`;
+  document.getElementById('artistName').innerText = artist;
 }
